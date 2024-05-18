1a3e068707472aee2d3e71cac65d53e38d36752b
Braulio Oliveira <brauliobo@gmail.com>
2024-05-18 00:12:39 -0300

    py/inotify: only watch if same device and directories to reduce time and memory usage

--- lib/ruby-livesync/live_sync/py/inotify.py
+++ lib/ruby-livesync/live_sync/py/inotify.py
@@ -1,11 +1,13 @@
 import pyinotify
 import os
 import time
+import sys
 
 class EventHandler(pyinotify.ProcessEvent):
-    def my_init(self, excludes, recursive, mask):
+    def my_init(self, excludes, recursive, mask, base_device):
         self.excludes = excludes
         self.recursive = recursive
+        self.base_device = base_device
         self.mask = mask
         self.events = set()
 
@@ -21,15 +23,32 @@ class EventHandler(pyinotify.ProcessEvent):
                 print(f"{event_desc} {full_path}", flush=True)
             self.events.clear()
 
-    def add_watch(self, path):
-        wm.add_watch(path, self.mask, rec=self.recursive, auto_add=True)
+    def watch_path(self, path):
+        if not any(exclude in path for exclude in self.excludes):
+            try:
+                if os.stat(path).st_dev == self.base_device:
+                    wm.add_watch(path, self.mask, auto_add=True)
+                else:
+                    print(f"Skipping {path} (different device)", file=sys.stderr)
+            except Exception as e:
+                print(f"Error adding watch on {path}: {e}", file=sys.stderr)
 
-excludes = [%{excludes}]
-recursive = %{recursive}
-event_mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_TO | pyinotify.IN_DELETE  # Event mask
+    def add_watch(self, path):
+        if self.recursive:
+            for root, dirs, _ in os.walk(path):
+                self.watch_path(root)
+                for dir in dirs:
+                    self.watch_path(os.path.join(root, dir))
+        else:
+            self.watch_path(path)
+
+excludes    = [%{excludes}]
+recursive   = %{recursive}
+event_mask  = pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_TO | pyinotify.IN_DELETE  # Event mask
+base_device = os.stat('%{path}').st_dev
 
 wm = pyinotify.WatchManager()
-handler = EventHandler(excludes=excludes, recursive=recursive, mask=event_mask)
+handler = EventHandler(excludes=excludes, recursive=recursive, mask=event_mask, base_device=base_device)
 notifier = pyinotify.Notifier(wm, handler)
 handler.add_watch('%{path}')
 
